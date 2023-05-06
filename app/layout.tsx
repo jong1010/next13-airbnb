f53cc1fe583f81e40d24ae6c8a23dd2a47db1b4c
jon <130399919+jong1010@users.noreply.github.com>
2023-05-06 10:46:47 +0000

    Add tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,16 @@
+import { Inter } from "next/font/google"
+
+export const metadat = {
+  title: "Airbnb",
+  description: "Airbnb clone"
+}
+
+const font = Inter({ subsets: ["latin"] })
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <h1>Hello World!</h1>;
+  return <h1 className="text-rose-500 text-2xl">Hello!</h1>;
 }
